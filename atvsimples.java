a3d26d4901e675bf697eaf95e70dcf8980ac7367
Regis Costa Pedro <138240650+RegisCostaPedro@users.noreply.github.com>
2023-06-30 14:17:43 -0300

    Add files via upload

---
+++ atvsimples.java
@@ -0,0 +1,181 @@
+import java.util.*;
+
+           //1. Escreva um programa que imprima a mensagem "Meu nome é [seu nome]".
+public class atvsimples {
+
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Atividade 1 ");
+        System.out.println();
+
+        String nome = "Regis";
+
+        System.out.println("Meu nome é " + nome);
+
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Atividade 2 ");
+        System.out.println();
+
+        int n1=2, n2=2, soma;
+
+        soma = n1 + n2;
+
+        System.out.println("A soma de 2+2 é igual a: " + soma);
+
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Atividade 3 ");
+        System.out.println();
+
+        System.out.println("**********");
+
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Atividade 4 ");
+        System.out.println();
+
+        System.out.println("*****");
+        System.out.println("*   *");
+        System.out.println("*   *");
+        System.out.println("*   *");
+        System.out.println("*****");
+
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Atividade 5 ");
+        System.out.println();
+
+        System.out.println("     #");
+        System.out.println("    ###");
+        System.out.println("   ######");
+        System.out.println("  #########");
+        System.out.println(" ############");
+        System.out.println("###############");
+
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Atividade 6 ");
+        System.out.println();
+
+        System.out.println("     .");
+        System.out.println("    ...");
+        System.out.println("   ......");
+        System.out.println("  .........");
+        System.out.println(" ............");
+        System.out.println("...............");
+        System.out.println(" ............");
+        System.out.println("   .........");
+        System.out.println("    ......");
+        System.out.println("      ...");
+        System.out.println("       .");
+
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Atividade 7 ");
+
+        System.out.println("     $");
+        System.out.println("    $$$");
+        System.out.println("   $$$$$$");
+        System.out.println("  $$$$$$$$$");
+        System.out.println(" $$$$$$$$$$$$");
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Atividade 8 ");
+
+        System.out.println("     *");
+        System.out.println("    ***");
+        System.out.println("   ******");
+        System.out.println("  *********");
+        System.out.println(" ************");
+        System.out.println("***************");
+        System.out.println("    *****");
+        System.out.println("    *****");
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Atividade 9 ");
+
+        System.out.println("      *            *  ");
+        System.out.println("   *       *     *     *");
+        System.out.println("   *          *        *");
+        System.out.println("    *                 *");
+        System.out.println("     *               *");
+        System.out.println("        *          *");
+        System.out.println("           *    *  ");
+        System.out.println("             *      ");
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Atividade 10 ");
+
+
+        System.out.println("  ...... ");
+        System.out.println("..........");
+        System.out.println("  ......   ");
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Atividade 11 ");
+
+        System.out.println("   >");
+        System.out.println("     >");
+        System.out.println("       >");
+        System.out.println("     >");
+        System.out.println("   >");
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Atividade 12 ");
+
+        System.out.println("    <");
+        System.out.println("  <");
+        System.out.println("<");
+        System.out.println("  <");
+        System.out.println("    <");
+
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Atividade 13 ");
+
+        System.out.println("     *    ");
+        System.out.println("    ***    ");
+        System.out.println("***********");
+        System.out.println("  **   ** ");
+        System.out.println(" *        *");
+
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Atividade 14 ");
+
+        System.out.println("####    ");
+        System.out.println("    ####");
+        System.out.println("####    ");
+        System.out.println("    ####");
+        System.out.println("####    ");
+        System.out.println("    ####");
+        System.out.println("####    ");
+        System.out.println("    ####");
+
+
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Atividade 15");
+
+        System.out.println(" ********************");
+        System.out.println("  *****************");
+        System.out.println("    **************");
+        System.out.println("     ***********");
+        System.out.println("       ********");
+        System.out.println("        *****");
+        System.out.println("          *");
+
+        System.out.println("-----------------------------FIM!-----------------------------------------");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
